ed67a4c914210f157f6b2643f8f82cb15468c605
thaninaadda <83210394+thaninaadda@users.noreply.github.com>
2023-02-22 21:49:53 -0500

    ajout des informations dans le init

---
+++ Personne.py
@@ -0,0 +1,12 @@
+
+
+
+class Personne:
+    def __init__(self, nom, adresse, ville, province, codePostal, telephone, courriel):
+        self.nom = nom
+        self.adresse = adresse
+        self.ville = ville
+        self.province = province
+        self.codePostal = codePostal
+        self.telephone = telephone
+        self.courriel = courriel
