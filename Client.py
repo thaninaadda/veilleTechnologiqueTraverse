4cbd122966240b131ef453d0a33bd2898b081266
thaninaadda <83210394+thaninaadda@users.noreply.github.com>
2023-02-22 21:58:44 -0500

    ajout de l'heritage au niveau de la classe client

--- Client.py
+++ Client.py
@@ -1,15 +1,14 @@
-class Client:
-    def __init__(self, nom, adresse, ville, province, codePostal, telephone, courriel):
-        self.nom = nom
-        self.adresse = adresse
-        self.ville = ville
-        self.province = province
-        self.codePostal = codePostal
-        self.telephone = telephone
-        self.courriel = courriel
-        self.numeroIdentification : numeroIdentification
-        self.sexe= sexe
-        self.dateDeNaissance= dateDeNaissance
+
+
+class Client(Personne):
+    def __init__(self, nom, adresse, ville, province, codePostal, telephone, courriel, numeroIdentification, sexe,dateDeNaissance):
+        super().__init__(nom, adresse, ville, province, codePostal, telephone, courriel)
+        self.numeroIdentification = numeroIdentification
+        self.sexe = sexe
+        self.dateDeNaissance = dateDeNaissance
+
+
+
 
 
         def GetHashCode(self):
