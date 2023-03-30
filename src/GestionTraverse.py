4d5fc5e0fc31e61fbf2927b0b3f44ba6360ad6fd
thaninaadda <83210394+thaninaadda@users.noreply.github.com>
2023-03-30 03:18:44 -0400

    ajout des commentaires dans GestionTraverse

--- src/GestionTraverse.py
+++ src/GestionTraverse.py
@@ -1,7 +1,7 @@
 #
-# Nom du Fichier : tkinterApp.py
+# Nom du Fichier : GestionTraverse.py
 # Auteur: Thanina Adda
-# But: contient toutes les interfaces.
+# But: Permet de faire appel aux autres classes et aux intérfaces pour pouvoir ajouter des clients, Employés et véhicules.
 #
 #
 import tkinter as tk
@@ -12,57 +12,52 @@ from Traversier import Traversier
 from Type import Type
 from Client import Client
 from Employe import Employe
-# from Personne import Personne
 
 
-# represente la police choisit pour le projet .
+
+# Représente la police choisit pour le projet .
 LARGEFONT =("Verdana", 20)
 
-# Classe tkinterApp, cette classe appel la page Principale au debut du programme.
+# Classe GestionTraverse, cette classe appel la page Principale au debut du programme.
 class GestionTraverse(tk.Tk):
     
-    # __init__ function for class tkinterApp
+    # __init__ function de la classe GestionTraverse
     def __init__(self, *args, **kwargs):
-        # __init__ function for class Tk
+        # __init__ function  de la classe Tk
         tk.Tk.__init__(self, *args, **kwargs)
         
-        # creating a container
+        # Création du container
         container = tk.Frame(self)
         container.pack(side = "top", fill = "both", expand = True)
 
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
 
-        # initializing frames to an empty array
+        # Intialisation du frame
         self.frames = {}
 
-        # iterating through a tuple consisting
-        # of the different page layouts
         for F in (StartPage,Type,Vehicule,Traverse,Client,Employe,Traversier):
 
             frame = F(container, self)
 
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
+            # appel des pages 
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky ="nsew")
 
         # démarrage de la page StartPage
         self.pageDemarrage()
 
+    #Ouverture de la page StartPage
     def pageDemarrage(self):
         self.show_frame(StartPage)
 
-    # to display the current frame passed as
-    # parameter
+    
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
                 
 # Page StartPage
-
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
        
@@ -73,41 +68,37 @@ class StartPage(tk.Frame):
         label = ttk.Label(self, text="projet Traverse", font=LARGEFONT)
         label.grid(row=0, column=4, padx=10, pady=10)
 
-		# Bouton Type Traverse qui appel justement la page Type Traverse.
+		# Bouton Type Traverse qui appel la page Type Traverse.
         button1 = ttk.Button(self, text="Type Traverse",
                              command=lambda: controller.show_frame(Type))
         button1.grid(row=1, column=6, padx=10, pady=10)
 
-        # Bouton Type Traverse qui appel justement la page Vehicule.
+        # Bouton Type Traverse qui appel la page Vehicule.
         button2 = ttk.Button(self, text="Vehicule",
                              command=lambda: controller.show_frame(Vehicule))
         button2.grid(row=2, column=6, padx=10, pady=10)
         
-        # Bouton Type Traverse qui appel justement la page Traverse.
+        # Bouton Type Traverse qui appel la page Traverse.
         button3 = ttk.Button(self, text="Traverse",
                              command=lambda: controller.show_frame(Traverse))
         button3.grid(row=3, column=6, padx=10, pady=10)
         
-        # Bouton Type Traverse qui appel justement la page Client.
-
-
+        # Bouton Type Traverse qui appel la page Client.
         button4 = ttk.Button(self, text="Client",
                              command=lambda: controller.show_frame(Client))
         button4.grid(row=4, column=6, padx=10, pady=10)
 
-        # Bouton Type Traverse qui appel justement la page Employee.
-
-
+        # Bouton Type Traverse qui appel la page Employee.
         button5 = ttk.Button(self, text="Employee",
                              command=lambda: controller.show_frame(Employe))
         button5.grid(row=5, column=6, padx=10, pady=10)
 
-        # Bouton Type Traverse qui appel justement la page Traversier.
+        # Bouton Type Traverse qui appel la page Traversier.
         button6 = ttk.Button(self, text="Traversier",
                              command=lambda: controller.show_frame(Traversier))
         button6.grid(row=6, column=6, padx=10, pady=10)
 
-# Driver Code
+
 app = GestionTraverse()
 app.mainloop()
 
